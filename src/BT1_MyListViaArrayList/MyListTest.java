cafafcc64bc3a7b728836c2831b0379bb156cf4f
Minh <congchuabe2000@gmail.com>
2023-03-20 17:13:33 +0700

    BT

---
+++ src/BT1_MyListViaArrayList/MyListTest.java
@@ -0,0 +1,41 @@
+package BT1_MyListViaArrayList;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> myList = new MyList<>(5);
+        System.out.println("Inintial array size: " + myList.size());
+        myList.add(0, 15);
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(10);
+        myList.add(100);
+        System.out.println("After array size: " + myList.size());
+        System.out.println(myList.get(0));
+        System.out.println(myList.get(1));
+        System.out.println(myList.get(2));
+        System.out.println(myList.get(3));
+        System.out.println(myList.get(4));
+        System.out.println(myList.get(5));
+        System.out.println(myList.get(6));
+        System.out.println(myList.get(7));
+        myList.add(2, 23);
+        myList.remove(5);
+
+        System.out.println("Output result =>> "+myList.size());
+        System.out.println(myList.get(0));
+        System.out.println(myList.get(1));
+        System.out.println(myList.get(2));
+        System.out.println(myList.get(3));
+        System.out.println(myList.get(4));
+        System.out.println(myList.get(5));
+        System.out.println(myList.get(6));
+        System.out.println(myList.get(7));
+
+        System.out.println("Contain of value 10: " + myList.contains(10));
+        System.out.println("Index of value 10: " + myList.indexOf(10));
+        System.out.println("Contain of value 90: " + myList.contains(90));
+    }
+}
